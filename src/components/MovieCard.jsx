9f1853234fdfdaf692e221511244d3f59c25125e
daniel <danielcatlover8856@gmail.com>
2023-09-05 20:48:59 -0600

    add images of the api

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,9 @@
-export function MovieCard() {
+export function MovieCard({movie}) {
+  const imageUrl ="https://image.tmdb.org/t/p/w500"+movie.poster_path;
+  
   return (
-    <h1>MovieCard</h1>
-  )
+    <li>
+      <img width={230} height={345} src={imageUrl} alt={movie.title}/>
+    </li>
+  );
 }
-
