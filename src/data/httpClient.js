373ac9c5b3b33a2c3b8ed0acf873350445c246e6
daniel <danielcatlover8856@gmail.com>
2023-09-05 18:55:29 -0600

    fixed bug of the http client

--- src/data/httpClient.js
+++ src/data/httpClient.js
@@ -1,9 +1,9 @@
-const API = "https://api.themoviedb.org/";
+const API = "https://api.themoviedb.org/3";
 export function get(path){
     return fetch(API+path,{
         headers: {
-            Authorization: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NWI0ZWQxYmQwOTM1MzE4OWE2ZDMxMGVlNzkyNzY1NyIsInN1YiI6IjY0ZjU1OGRmN2Q0MWFhMDBlMThhYTEwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SPictiY9VBnt_tE0gMAQ49OOfH2j5lfva15N0mUyipU",
-            "Content-type": "application/json;charsert=utf-8"
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NWI0ZWQxYmQwOTM1MzE4OWE2ZDMxMGVlNzkyNzY1NyIsInN1YiI6IjY0ZjU1OGRmN2Q0MWFhMDBlMThhYTEwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SPictiY9VBnt_tE0gMAQ49OOfH2j5lfva15N0mUyipU",
+            "Content-type": "application/json;charset=utf-8"
         }
     }).then((result) => result.json())
 }
