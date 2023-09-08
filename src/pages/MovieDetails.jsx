fb1a3240b1184f902e345855ccc200d9ce5d847d
Christopher <chrismartinez.2054@gmail.com>
2023-09-08 02:06:14 -0600

    add get movie image component

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,5 +1,19 @@
-export function MovieDetails(){
-    return(
-        <h1>movie details</h1>
-    )
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { get } from "../data/httpClient";
+
+
+export function MovieDetails() {
+  const { movieId } = useParams;
+  const [movie, SetMovie] = useState (null);
+  useEffect(() =>{
+    get("/movie/"+movieId).then((data) => {
+        SetMovie(data)
+    })
+  },[movieId])
+   
+return(
+    <div></div>
+)
 }
+
