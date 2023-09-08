3a9fecd6d4f51705514056be64a74d91169b6642
Christopher <chrismartinez.2054@gmail.com>
2023-09-08 02:42:08 -0600

    bug fixed

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,19 +1,22 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { get } from "../data/httpClient";
-
+import { getMovieImg } from "../utils/getMovieImage";
 
 export function MovieDetails() {
-  const { movieId } = useParams;
-  const [movie, SetMovie] = useState (null);
-  useEffect(() =>{
-    get("/movie/"+movieId).then((data) => {
-        SetMovie(data)
-    })
-  },[movieId])
-   
-return(
-    <div></div>
-)
-}
+  const { movieId } = useParams();
+  const [movie, SetMovie] = useState([]);
 
+  useEffect(() => {
+    get("/movie/" + movieId).then((data) => {
+      SetMovie(data);
+    });
+  }, [movieId]);
+  const imageUrl = getMovieImg(movie.poster_path, 500);
+
+  return (
+    <div>
+      <img src={imageUrl} alt={movie.title} />
+    </div>
+  );
+}
